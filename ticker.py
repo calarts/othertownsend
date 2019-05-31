a33fac1968555b82d5fac3ccfa9e4251e941c516
Douglas Goodwin <goodwind@metro.net>
2019-05-30 22:26:15 -0700

    wrong address

--- ticker.py
+++ ticker.py
@@ -174,8 +174,8 @@ def main():
 
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    logger.handlers = gunicorn_logger.handlers
+    logger.setLevel(gunicorn_logger.level)
 
 if __name__ == '__main__':
     main()
