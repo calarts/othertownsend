2daa35c75cedb5267f34308761de89287b2ea8db
Douglas Goodwin <goodwind@metro.net>
2019-05-30 23:29:25 -0700

    logger typo

--- ticker.py
+++ ticker.py
@@ -31,12 +31,12 @@ from telegram.ext import Updater, CommandHandler
 from _config import TOKEN
 
 # Enable logging
-logging.setLevel(logging.DEBUG)
 logging.FileHandler('tickererror.log')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 heartrate_file = "data/heart_rate-2019-01-06.json"
 pulserecord = {}
