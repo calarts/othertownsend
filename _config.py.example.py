36b8e2f1ec88365c922dd38a550f956c45620fc6
Douglas Goodwin <goodwind@metro.net>
2019-05-31 08:40:13 -0700

    example configfile

---
+++ _config.py.example.py
@@ -0,0 +1,10 @@
+# RENAME THIS FILE TO _config.py AND PUT A REAL TELEGRAM TOKEN INSIDE
+# devothertowbsend
+# t.me/devothertowbsend_bot
+# TOKEN = "627805063:kpqwouepkjdlnasdmnlamsncv;lnsdc" 
+
+# other.townsend
+# t.me/OtherTownsendBot
+# /setinline 
+# /setuserpic
+TOKEN = "746808563:klsadlkjasldkjlaksjdlasd" 
