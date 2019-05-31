ed0b24880964c111ddb542d2a4aa044ecce7d4f7
Douglas Goodwin <goodwind@metro.net>
2019-05-30 22:25:13 -0700

    add gunicorn logging

--- ticker.py
+++ ticker.py
@@ -172,6 +172,11 @@ def main():
 	# non-blocking and will stop the bot gracefully.
 	updater.idle()
 
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
 if __name__ == '__main__':
     main()
 
