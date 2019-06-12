ba2cff4858ecb0d4edfb34b49c2981be8bb2a723
Douglas Goodwin <goodwind@metro.net>
2019-06-12 01:02:09 -0700

    move those recordconvs arounf

--- ticker.py
+++ ticker.py
@@ -162,9 +162,6 @@ def recordconvo(message):
 
 def reply_withfeeling(update, context):
     """How do you feel?"""
-
-    recordconvo(update.message)
-
     mypulse = gimmebeats(heartrate_keylist)
     mood = gimmeFeelings()[2]
     if mood == 1:
@@ -196,13 +193,11 @@ def reply_withfeeling(update, context):
 
 def reply_withsleep(update, context):
     """How did you sleep?"""
-    recordconvo(update.message)
     msg = str(gimmeFeelings()[1])
     update.message.reply_text(msg)
 
 def reply_withphoto(update,context):
     """Where are you? Send a photo of a place."""
-    recordconvo(update.message)
     imgs = ["media/37.64961_-122.45323.jpg",
             "media/37.7919_-122.4038.jpg",
             "media/37.914996_-122.533479.jpg"
@@ -218,7 +213,6 @@ def reply_withphoto(update,context):
 
 def reply_withhtml(update,context):
     """What are you looking at?"""
-    recordconvo(update.message)
     looklist = gimmecurrlooks()
     lk = choice(looklist)
     update.message.reply_html( str(lk) )
@@ -227,15 +221,10 @@ def reply_withhtml(update,context):
 def main():
     # """Run bot."""
 
-
-
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
-
-
-
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     # Let's listen for specific questions:
@@ -245,6 +234,8 @@ def main():
     # What are you looking at??
     class FilterLook(BaseFilter):
         def filter(self, message):
+            print(FilterLook)
+            recordconvo(message)
             return 'looking at?' in message.text
 
     filter_look = FilterLook()
@@ -255,11 +246,15 @@ def main():
     # Where are you?
     class FilterWhere(BaseFilter):
         def filter(self, message):
+            print(FilterWhere)
+            recordconvo(message)
             return 'Where' in message.text
 
     # Where are you?
     class FilterWheresimple(BaseFilter):
         def filter(self, message):
+            print(FilterWheresimple)
+            recordconvo(message)
             return 'where' in message.text
 
     filter_where = FilterWhere()
@@ -273,14 +268,20 @@ def main():
 
     class FilterFeel(BaseFilter):
         def filter(self, message):
+            print(FilterFeel)
+            recordconvo(message)
             return 'you feel?' in message.text
 
     class FilterFeeling(BaseFilter):
         def filter(self, message):
+            print(FilterFeeling)
+            recordconvo(message)
             return 'you feeling?' in message.text
 
     class DayBeen(BaseFilter):
         def filter(self, message):
+            print(FilterLook)
+            recordconvo(message)
             return 'day been?' in message.text
 
     filter_feel = FilterFeel()
@@ -293,6 +294,8 @@ def main():
     # How did you sleep? (sleep)
     class FilterSleep(BaseFilter):
         def filter(self, message):
+            print(FilterSleep)
+            recordconvo(message)
             return 'you sleep?' in message.text
 
     filter_sleep = FilterSleep()
